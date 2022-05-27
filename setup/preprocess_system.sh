a61c45a34629d358e30020f828dca915e4ecf5ac
Alexia D <alexia.dormann@epfl.ch>
2022-05-27 09:22:07 +0200

    Update preprocess_system.sh

--- setup/preprocess_system.sh
+++ setup/preprocess_system.sh
@@ -1,5 +1,7 @@
 #Load proper amber tools
-module load amber
+module load gcc/8.4.0
+module load mvapich2/2.3.4
+module load amber/18.20-mpi
 
 #Generate .mol2 and .frcmod files for the ligand
 antechamber -fi pdb -fo mol2 -i inhibitor.pdb -o inhibitor.mol2 -c bcc -nc 0 -m 1
