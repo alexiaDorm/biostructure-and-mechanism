2e0b45912cfdb83baa9b6ce8ee78395ad9e23dca
Alexia D <alexia.dormann@epfl.ch>
2022-05-29 20:04:28 +0200

    Update preprocess_system.sh

--- setup/preprocess_system.sh
+++ setup/preprocess_system.sh
@@ -11,4 +11,4 @@ parmchk2 -f mol2 -i inhibitor.mol2 -o inhibitor.frcmod
 cat KRAS_G12C_Y96C.pdb inhibitor.pdb > KRAS_inhibitor.pdb
 
 #Generate topology and coordinate files of the system
-tleap11 -f KRAS_tleap.in > KRAS_tleap.out
+tleap -f KRAS_tleap.in > KRAS_tleap.out
